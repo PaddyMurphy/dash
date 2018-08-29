014071749187dbee6121399ab6f7b22321eeb323
PaddyMurphy <patrickblewis@gmail.com>
2018-08-29 17:09:51 -0500

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {simpleAction} from './actions/Actions.js';
-import logo from './logo.svg';
-import './App.css';
+import {bindActionCreators} from 'redux';
+import * as actions from './actions/Actions.js';
+import './styles/App.css';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles';
+import withRoot from './withRoot';
+
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+});
 
 class App extends Component {
+  state = {
+    open: false,
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
   simpleAction = event => {
     this.props.simpleAction();
   };
 
   render() {
+    const {classes} = this.props;
+    const {open} = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div className={classes.root}>
+        <Dialog open={open} onClose={this.handleClose}>
+          <DialogTitle>Super Secret Password</DialogTitle>
+          <DialogContent>
+            <DialogContentText>1-2-3-4-5</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.handleClose}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Typography variant="display1" gutterBottom>
+          Material-UI
+        </Typography>
+        <Typography variant="subheading" gutterBottom>
+          example project
+        </Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={this.handleClick}>
+          Super Secret Password
+        </Button>
         <p className="App-intro">
-          <button onClick={this.simpleAction}>Test redux action</button>
+          <Button onClick={this.simpleAction}>Test redux action</Button>
         </p>
 
         <pre>{JSON.stringify(this.props)}</pre>
@@ -30,8 +85,17 @@ const mapStateToProps = state => ({
   ...state,
 });
 
-const mapDispatchToProps = dispatch => ({
-  simpleAction: () => dispatch(simpleAction()),
-});
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      ...actions,
+    },
+    dispatch,
+  );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default withRoot(withStyles(styles)(Index));
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withRoot(withStyles(styles)(App)),
+);
